ce862ab527e769f06dda612cdda5e5adf758eeb4
Manav shah <takedownstew766@gmail>
2026-06-26 08:27:17 +0000

    Lab 1 finished

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,21 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node *slow = head, *fast = head;
+    
+   
+    while (fast && fast->next) {
+        
+       
+        fast = fast->next->next;
+        slow = slow->next;
+        
+        
+        if (slow == fast) {
+            return 1;
+        }
+    }
+    
+   
     return 0;
 }
